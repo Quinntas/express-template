60d72876e17ed235426a57093ceacfee0c1e0277
Caio Quintas <caioquintassantiago@gmail.com>
2024-02-04 12:01:18 -0300

    feat: query caching

--- src/core/repo/baseRepo.ts
+++ src/core/repo/baseRepo.ts
@@ -1,6 +1,7 @@
 import {Spectre} from "spectre-orm";
 import {Primitive} from "../../types/primitives";
 import {RedisClient} from "../../utils/redisClient";
+import {hashString} from "../../utils/encryption";
 
 interface QueryExecutor {
     query: string;
@@ -14,7 +15,29 @@ interface QueryExecutor {
     }
 }
 
+async function handleCache(queryExecutor: QueryExecutor) {
+    const key = queryExecutor.cache.key || hashString(queryExecutor.query);
+
+    const cachedResult = await queryExecutor.cache.redisInstance.get(key);
+
+    if (cachedResult)
+        return JSON.parse(cachedResult);
+
+    const result = await executeQuery({
+        query: queryExecutor.query,
+        params: queryExecutor.params,
+        spectreInstance: queryExecutor.spectreInstance
+    });
+
+    await queryExecutor.cache.redisInstance.set(key, JSON.stringify(result));
+
+    return result;
+}
+
 export async function executeQuery(queryExecutor: QueryExecutor) {
+    if (queryExecutor.cache)
+        return await handleCache(queryExecutor);
+
     return await queryExecutor.spectreInstance.strategy.rawQuery({
         query: queryExecutor.query,
         values: queryExecutor.params
