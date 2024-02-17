ca4eae92773265deacb7721b4af32445e3e3db81
Caio "Quintas_" Santiago <caioquintassantiago@gmail.com>
2024-02-17 14:49:19 -0300

    Update userPassword.test.ts

--- src/modules/user/domain/valueObjects/userPassword.test.ts
+++ src/modules/user/domain/valueObjects/userPassword.test.ts
@@ -1,5 +1,4 @@
 import {expect, test} from "vitest";
-import {validateUserName} from "./userName";
 import {validateUserPassword} from "./userPassword";
 
 test("ValueObject - UserPassword - Invalid password less than 6 chars", () => {
@@ -10,12 +9,12 @@ test("ValueObject - UserPassword - Invalid password less than 6 chars", () => {
 
 test("ValueObject - UserPassword - Invalid name more than 20 chars", () => {
     expect(() => {
-        validateUserName('123456'.repeat(10))
+        validateUserPassword('123456'.repeat(10))
     }).toThrowError()
 })
 
 test("ValueObject - UserPassword - Valid password", () => {
-    const result = validateUserName('123456')
+    const result = validateUserPassword('123456')
     expect(result).toBeDefined()
     expect(result).toBe('123456')
 })
