3823bd5df66d7b683548f67136a983de91cbc990
Caio "Quintas_" Santiago <caioquintassantiago@gmail.com>
2024-03-23 20:21:05 -0300

    feat: add prettier default config and prettify before commit

---
+++ prettier.config.js
@@ -0,0 +1,13 @@
+/** @type {import("prettier").Config} */
+const config = {
+    trailingComma: "es5",
+    tabWidth: 4,
+    semi: true,
+    singleQuote: true,
+    bracketSpacing: false,
+    bracketSameLine: true,
+    arrowParens: "always",
+    endOfLine: "lf",
+};
+
+export default config;
