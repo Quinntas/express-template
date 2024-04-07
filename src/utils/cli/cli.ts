6f38f5bebc6e025a6f58b658810a337c350bdd48
Caio "Quintas_" Santiago <caioquintassantiago@gmail.com>
2024-04-07 17:20:56 -0300

    feat: cli create use case

--- src/utils/cli/cli.ts
+++ src/utils/cli/cli.ts
@@ -4,10 +4,17 @@ import {domainTemplate} from './domainTemplate';
 import {mapperTemplate} from './mapperTemplate';
 import {repoTemplate} from './repoTemplate';
 import {routerTemplate} from './routerTemplate';
+import {useCaseDTOTemplate} from "./useCaseDTOTemplate";
+import {useCaseTemplate} from "./useCaseTemplate";
+
+function parseArg(arg: string | null): arg is string {
+    if (!arg) throw new Error('No argument provided');
+    if (/\d/.test(arg)) throw new Error('Module name cannot contain numbers');
+    return true
+}
 
 function createModule(name: string | null) {
-    if (!name) throw new Error('No module name provided');
-    if (/\d/.test(name)) throw new Error('Module name cannot contain numbers');
+    if (!parseArg(name)) return;
 
     console.log(`Creating module: ${name}`);
 
@@ -52,12 +59,39 @@ function createModule(name: string | null) {
     console.log(`[Important] Don't forget to add the table name in the repo!`);
 }
 
+function createUseCase(modules: string | null, name: string | null) {
+    if (!parseArg(modules)) return;
+    if (!parseArg(name)) return;
+
+    console.log(`Creating use case: ${name} in module: ${modules}`);
+
+    const moduleName = modules.charAt(0).toLowerCase() + modules.slice(1);
+
+    if (!fs.existsSync(`src/modules/${moduleName}`)) throw new Error(`Module ${moduleName} does not exist`);
+
+    const useCaseName = name.charAt(0).toLowerCase() + name.slice(1);
+
+    console.log(`Creating use case structure for ${useCaseName}`);
+
+    fs.mkdirSync(`src/modules/${moduleName}/useCases/${useCaseName}`);
+
+    const fDTO = fs.createWriteStream(`src/modules/${moduleName}/useCases/${useCaseName}/${useCaseName}DTO.ts`);
+    fDTO.write(useCaseDTOTemplate(useCaseName))
+
+    const fUseCase = fs.createWriteStream(`src/modules/${moduleName}/useCases/${useCaseName}/${useCaseName}UseCase.ts`);
+    fUseCase.write(useCaseTemplate(useCaseName))
+
+    console.log(`Use case ${useCaseName} created successfully`);
+}
+
 function main(args: string[]) {
     if (args.length === 0) throw new Error('No arguments provided');
 
     switch (args[0]) {
         case 'cm':
             return createModule(args[1]);
+        case 'cu':
+            return createUseCase(args[1], args[2]);
         default:
             throw new Error(`Unknown command: ${args[0]}`);
     }
