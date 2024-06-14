cf31e277a2f2fc6dee983924d0c499053fd97ae7
Quinntas <caioquintassantiago@gmail.com>
2024-06-14 15:12:41 -0300

    fix user table

--- src/modules/user/infra/database/userTable.ts
+++ src/modules/user/infra/database/userTable.ts
@@ -4,8 +4,7 @@ import {dbSchema} from '../../../shared/infra/database/schema';
 
 export const userTable = dbSchema.table('Users', {
     ...baseColumns,
-    name: varchar('name', {length: 50}).notNull(),
     email: varchar('email', {length: 191}).notNull(),
     password: varchar('password', {length: 191}).notNull(),
-    roleId: varchar('password', {length: 191}).notNull(),
+    role: varchar('password', {length: 191}).notNull(),
 });
